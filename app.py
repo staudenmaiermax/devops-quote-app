fbbcb7286494d9404a1eb5f7d01141a4a14bdc1d
Daymolock <maxstaudenmaier40@web.de>
2026-08-13 17:33:29 +0200

    Fix syntax error: return korrigiert

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ QUOTES = [
 @app.route('/')
 def home():
     quote = random.choice(QUOTES)
-    retun f"<h1>DevOps Quote of the Day:</h1><p>'{quote}'</p><p><i>Erstellt von Mages</i></p>"
+    return f"<h1>DevOps Quote of the Day:</h1><p>'{quote}'</p><p><i>Erstellt von Mages</i></p>"
 
 
 if __name__ == '__main__':
